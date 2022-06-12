ab88f532402bb7bdf99087bdf8022ed18f9cdec9
Michael <michaelsupercode@gmail.com>
2022-06-12 23:01:08 +0200

    third com

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FameList from './Components/ContactItem';
+import { Planet } from 'react-kawaii';
+
+function App() {
+  return (
+    <div className="App">
+      <FameList />
+    </div>
+  );
+}
+
+export default App;
+
